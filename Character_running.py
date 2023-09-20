35333f11067d7989c4c6cf4592a4f829086f260b
Junsoo Han <hanjs991124@gmail.com>
2023-09-20 17:59:44 +0900

    원 그리기-완

--- Character_running.py
+++ Character_running.py
@@ -3,27 +3,38 @@ import math
 import os
 
 os.chdir('D:\\2DGP\\Drill02')
-grass= lode_image('grass.png')
-character= lode_image('character.png')
-
 open_canvas()
+grass=load_image('grass.png')
+character=load_image('character.png')
 
 def run_circle():
     print('circle')
     clear_canvas_now()
     grass.draw_now(400,30)
     character.draw_now(400,90)
-    pass
+    delay(1)
+    
+    cx=800/2
+    cy=600/2
+    r=200
+    for deg in range(0,360,5):
+        x= cx+r*math.cos(deg/360*2*math.pi)
+        y= cy+r*math.sin(deg/360*2*math.pi)
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delay(0.1)
 
 
 def run_rectangle():
     print('rectangle')
-    
+    grass.draw_now(400,30)
+    character.draw_now(400,90)
     pass
 
 
 
-while true:
+while(True):
     run_circle()
     run_rectangle()
     break
