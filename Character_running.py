d5c5f6c680258fae29c9b966c590c66483810ddb
Junsoo Han <hanjs991124@gmail.com>
2023-09-20 17:43:26 +0900

    뼈대 완성

--- Character_running.py
+++ Character_running.py
@@ -1,4 +1,31 @@
 from pico2d import*
+import math
+import os
+
+os.chdir('D:\\2DGP\\Drill02_Good')
+grass=lode_image('grass.png')
+character=lode_image('character.png')
+
 open_canvas()
 
+def run_circle():
+    print('circle')
+    pass
+
+
+def run_rectangle():
+    print('rectangle')
+    pass
+
+
+
+while true:
+    run_circle()
+    run_rectangle()
+    break
+
+
+
 close_canvas
+
+
