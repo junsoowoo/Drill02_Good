4dc21ff2c94cbd4d5fb873a954656ba1516a6480
Junsoo Han <hanjs991124@gmail.com>
2023-09-20 18:13:58 +0900

    렌더함수 만듬

--- Character_running.py
+++ Character_running.py
@@ -8,6 +8,12 @@ grass=load_image('grass.png')
 character=load_image('character.png')
 
 
+
+def render_All(x,y):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,y)
+    delay(0.01)
     
 def run_circle():
     print('circle')
@@ -18,10 +24,7 @@ def run_circle():
     for deg in range(0,360,5):
         x= cx+r*math.cos(deg/360*2*math.pi)
         y= cy+r*math.sin(deg/360*2*math.pi)
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        delay(0.1)
+        render_All(x,y)
 
 
 def run_rectangle():
@@ -29,11 +32,10 @@ def run_rectangle():
 
     #bottom line
     for x in range(50,750+1,10):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(1)
-    
+        render_All(x,90)
+    #top line
+    for x in range(750,50,-10):
+        render(x,90)
 
 
 
