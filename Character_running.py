4c4c5a777d96c33c40f2857a0bbdea172a332d9d
Junsoo Han <hanjs991124@gmail.com>
2023-09-20 17:46:26 +0900

    기본 설정

--- Character_running.py
+++ Character_running.py
@@ -2,19 +2,23 @@ from pico2d import*
 import math
 import os
 
-os.chdir('D:\\2DGP\\Drill02_Good')
-grass=lode_image('grass.png')
-character=lode_image('character.png')
+os.chdir('D:\\2DGP\\Drill02')
+grass= lode_image('grass.png')
+character= lode_image('character.png')
 
 open_canvas()
 
 def run_circle():
     print('circle')
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(400,90)
     pass
 
 
 def run_rectangle():
     print('rectangle')
+    
     pass
 
 
